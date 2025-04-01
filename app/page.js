88e7e9e06f9e9f3e03cae7eeba5088811575c888
ImenZe <imenze4@gmail.com>
2025-04-01 17:16:43 +0200

    Initial commit: Application de lecture d'histoires

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+import StoryReader from '../src/components/StoryReader';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-blue-50">
+      <StoryReader />
+    </div>
+  );
+} 
